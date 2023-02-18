ba25d27b0bb4e234e85040157c58aaedfeb28414
hyunsu00 <hyun955807@naver.com>
2023-02-18 17:55:31 +0900

    pdf.js 빌드환경 구성

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # morfhis-unit-app-pdf-viewer
+
+## 패키지 인스톨
+```bash
+# 1. pdf.js 관련 패키지 설치 (실행경로 : CloudOffice/webpdf/)
+npm install -g gulp-cli
+cd ./extern/pdf.js && npm install --ignore-scripts --force
+cd ../../
+
+
+```
--- src/Components/PdfViewer/Content/WebPDF/pdfjs/pdfjs.js
+++ src/Components/PdfViewer/Content/WebPDF/pdfjs/pdfjs.js
@@ -1,19 +1,19 @@
 
 const import_pdfjs = () => {
   console.log(`[import_pdfjs() Begin]`);
-  import(/* webpackIgnore: true */ `${process.env.PUBLIC_URL}/external/pdfjs/web/viewer.js`) // 절대경로
+  import(/* webpackIgnore: true */ `${process.env.PUBLIC_URL}/libs/pdfjs/web/viewer.js`) // 절대경로
     .then((module) => {
       const _appOptions = window.PDFViewerApplicationOptions;
       _appOptions.set(
         "workerSrc",
-        `${process.env.PUBLIC_URL}/external/pdfjs/build/pdf.worker.js`
+        `${process.env.PUBLIC_URL}/libs/pdfjs/build/pdf.worker.js`
       ); // 절대경로
       _appOptions.set(
         "defaultUrl",
-        `${process.env.PUBLIC_URL}/external/pdfjs/web/compressed.tracemonkey-pldi-09.pdf`
+        `${process.env.PUBLIC_URL}/libs/pdfjs/web/compressed.tracemonkey-pldi-09.pdf`
       ); // 절대경로
 
-      console.log(`[import() Succeeded] : ${process.env.PUBLIC_URL}/external/pdfjs/web/viewer.js`);
+      console.log(`[import() Succeeded] : ${process.env.PUBLIC_URL}/libs/pdfjs/web/viewer.js`);
     })
     .catch((err) => {
       console.log(`[import() Failed] : ${err.message}`);
