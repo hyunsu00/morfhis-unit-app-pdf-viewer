667e0bd084ef43088f1430665ec8a03525ae1260
hyunsu00 <hyunsu00@hancom.com>
2023-05-07 17:44:45 +0900

    코드 수정

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,34 @@
-module.exports = {
-    webpack: function (config, env) {
-        config.module.rules.push({
-            test: /\.html$/i,
-			loader: 'html-loader',
-        });
-        return config;
+const { DefinePlugin } = require('webpack');
+
+const libsFiles = (() => {
+  function getFiles(dir, files_) {
+    files_ = files_ || [];
+    var files = require('fs').readdirSync(dir);
+    for (var i in files) {
+      var name = dir + '/' + files[i];
+      if (require('fs').statSync(name).isDirectory()) {
+        getFiles(name, files_);
+      } else {
+        files_.push(name.replace('./public', ''));
+      }
     }
+    return files_;
+  }
+
+  return getFiles('./public/libs');
+})();
+
+module.exports = {
+  webpack: function (config, env) {
+    config.module.rules.push({
+      test: /\.html$/i,
+      loader: 'html-loader',
+    });
+    config.plugins.push(
+      new DefinePlugin({
+        'process.env.LIBS_FILES': JSON.stringify(libsFiles)
+      })
+    );
+    return config;
+  },
 };
