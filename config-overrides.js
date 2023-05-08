65006b03346b202b5679d74de7990ee594d9fa77
hyunsu00 <hyunsu00@hancom.com>
2023-05-08 11:38:59 +0900

    코드 반영

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,38 @@
+const { DefinePlugin } = require('webpack');
+
+const addStaticFiles = (() => {
+  function getFiles(dir, files_) {
+    files_ = files_ || [];
+    var files = require('fs').readdirSync(dir);
+    for (var i in files) {
+      var name = dir + '/' + files[i];
+      if (require('fs').statSync(name).isDirectory()) {
+        getFiles(name, files_);
+      } else {
+        files_.push(name);
+      }
+    }
+    return files_;
+  }
+  const files = getFiles('./public/libs');
+  const addStaticFiles = files.map(file => {
+    return file.replace('./public', '');
+  });
+
+  return addStaticFiles;
+})();
+
 module.exports = {
   webpack: function (config, env) {
     config.module.rules.push({
       test: /\.html$/i,
       loader: 'html-loader',
     });
+    config.plugins.push(
+      new DefinePlugin({
+        'process.env.ADD_STATIC_FILES': JSON.stringify(addStaticFiles)
+      })
+    );
     return config;
   },
 };
